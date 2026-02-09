43381f741bc721a32cb8e738d6a1f0ca78d5cac9
arostami612 <arostami612@gmail.com>
2026-02-08 23:16:37 -0500

    ex 2

--- exo2.py
+++ exo2.py
@@ -20,11 +20,81 @@ Objectif :
 
 FACTEURS = [1.30, 1.15, 1.05, 0.95, 0.95, 1.05, 1.15, 1.30]
 
-# TODO: Lire 8 entiers (un par ligne) dans une liste personnes
+# Lire 8 entiers (un par ligne) dans une liste personnes
 #       En cas d'erreur de conversion ou valeur negative -> afficher le message d'erreur et quitter
+FACTEURS = [1.30, 1.15, 1.05, 0.95, 0.95, 1.05, 1.15, 1.30]
+try:
+    personnes = []
+    i = 0
+    while i < 8:
+        valeur = int(input())
+        personnes.append(valeur)
+        i = i + 1
+    
+    i = 0
+    invalide = False
+    while i < 8:
+        if personnes[i] < 0:
+            invalide = True
+        i = i + 1
+    
+    if invalide:
+        print("Erreur - donnees invalides.")
+    else:
 
-# TODO: Calculer les intensites brutes (liste de 8 floats)
+# Calculer les intensites brutes (liste de 8 floats)
+        intensites = []
+        i = 0
+        while i < 8:
+            intensite = personnes[i] * FACTEURS[i]
+            intensites.append(intensite)
+            i = i + 1
+# Calculer les niveaux normalises (liste de 8 entiers dans [0,10])
+        maxI = intensites[0]
+        i = 1
+        while i < 8:
+            if intensites[i] > maxI:
+                maxI = intensites[i]
+            i = i + 1
 
-# TODO: Calculer les niveaux normalises (liste de 8 entiers dans [0,10])
+        niveaux = []
+        if maxI == 0:
+            i = 0
+            while i < 8:
+                niveaux.append(0)
+                i = i + 1
+        else:
+            i = 0
+            while i < 8:
+                niveau = int((intensites[i] / maxI) * 10 + 0.5)
+                niveaux.append(niveau)
+                i = i + 1
 
-# TODO: Afficher la grille (10 lignes) puis la ligne des labels
+# Afficher la grille (10 lignes) puis la ligne des labels
+        ligne = 10
+        while ligne >= 1:
+            if ligne == 10:
+                print("10 | ", end="")
+            else:
+                print(" " + str(ligne) + " | ", end="")
+            
+            col = 0
+            while col < 8:
+                if niveaux[col] >= ligne:
+                    print("❚", end="")
+                else:
+                    print(".", end="")
+                
+                if col < 7:
+                    print(" ", end="")
+                else:
+                    print()
+                
+                col = col + 1
+            
+            ligne = ligne - 1
+        
+        print("     A B C D E F G H")
+        
+except ValueError:
+    print("Erreur - donnees invalides.")
